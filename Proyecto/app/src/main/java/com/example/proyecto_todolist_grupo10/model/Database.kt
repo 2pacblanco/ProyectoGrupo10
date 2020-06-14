f32a2bb0392a4b1532db0e69637f7deaf16b1f6a
johnny <jadonoso2@miuandes.cl>
2020-06-13 20:16:45 -0400

    some changes2

--- Proyecto/app/src/main/java/com/example/proyecto_todolist_grupo10/model/Database.kt
+++ Proyecto/app/src/main/java/com/example/proyecto_todolist_grupo10/model/Database.kt
@@ -1,13 +1,33 @@
 package com.example.proyecto_todolist_grupo10.model
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @Database(entities = [Users_table::class, Lists_table::class, Items_table::class],version = 1)
 @TypeConverters(LocalDateTimeConverter::class)
-abstract class Database: RoomDatabase(){
+abstract class AppDatabase: RoomDatabase(){
     abstract fun Users_tableDao(): Users_tableDao
     abstract fun Lists_tableDao(): Lists_tableDao
     abstract fun Items_tableDao(): Items_tableDao
+
+    companion object {
+        var INSTANCE: AppDatabase? = null
+
+        fun getAppDataBase(context: Context): AppDatabase? {
+            if (INSTANCE == null){
+                synchronized(AppDatabase::class){
+                    INSTANCE = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "myDB").build()
+                }
+            }
+            return INSTANCE
+        }
+
+        fun destroyDataBase(){
+            INSTANCE = null
+        }
+    }
+    
 }
