ede3263f603f0975e02b9ae3dd0e598b18f0c5da
life <ramartinez2@miuandes.cl>
2020-07-06 14:03:21 -0400

    google auth

--- Proyecto/app/src/main/java/com/example/proyecto_todolist_grupo10/MainActivity.kt
+++ Proyecto/app/src/main/java/com/example/proyecto_todolist_grupo10/MainActivity.kt
@@ -7,19 +7,30 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.proyecto_todolist_grupo10.configuration.api_key
 import com.example.proyecto_todolist_grupo10.model.Users
-import com.example.proyecto_todolist_grupo10.networking.ApiApi
-import com.example.proyecto_todolist_grupo10.networking.ApiService
 import com.example.proyecto_todolist_grupo10.networking.HerokuApi
 import com.example.proyecto_todolist_grupo10.networking.HerokuApiService
 import com.example.proyecto_todolist_grupo10.util.LocationUtil
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private val mAuth : FirebaseAuth by lazy {FirebaseAuth.getInstance()}
+    private val RC_GOOGLE_SIGN_IN = 99
+
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+
 
     companion object{
         var logUser : Users? = null
@@ -34,6 +45,13 @@ class MainActivity : AppCompatActivity() {
 
         fusedLocationClient1 = LocationServices.getFusedLocationProviderClient(this)
         ItemDetail.locationData = LocationUtil(this)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
         val user: Users? = intent.getSerializableExtra("usuario_conect") as? Users
 
@@ -64,22 +82,97 @@ class MainActivity : AppCompatActivity() {
         })
         println(WelcomeActivity.loginuser.toString())
 
-    }
 
-    fun onLogin(view: View){
-        val email: String = etMail.text.toString()
-        val pass: String = etPassword.text.toString()
-        if(logUser == null){
-            var intent = Intent(this,WelcomeActivity::class.java)
-            startActivity(intent)
-        }
-        else{
-            var intent = Intent(this,WelcomeActivity::class.java)
-            intent.putExtra("logUser",logUser)
-            startActivity(intent)
 
+        btLogin.setOnClickListener(){
+            val email = etMail.text.toString()
+            val password = etPassword.text.toString()
+            if ( isValidMailAndPassword(email, password)){
+                logInByEmailAndPassword(email,password)
+            }
         }
 
+        logInGoogle.setOnClickListener(){
+            view: View? -> signInGoogle()
+        }
     }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_GOOGLE_SIGN_IN)
+    }
+
+    private fun signOut() {
+        // Firebase sign out
+        mAuth.signOut()
+
+        // Google sign out
+        mGoogleSignInClient.signOut().addOnCompleteListener(this) {
+        }
+    }
+
+    private fun signInGoogle(){
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_GOOGLE_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_GOOGLE_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Toast.makeText(this, "firebaseAuthWithGoogle:", Toast.LENGTH_SHORT).show()
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(this, "Google sign in failed", Toast.LENGTH_SHORT).show()
+                // ...
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(this, "signInWithCredential:success", Toast.LENGTH_SHORT).show()
+                    val user = mAuth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this, "Authentication Failed", Toast.LENGTH_SHORT).show()
+                    // ...
+                }
+
+                // ...
+            }
+    }
+
+    private fun logInByEmailAndPassword(email: String, password: String){
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){task ->
+            if (task.isSuccessful){
+                Toast.makeText(this, "LogIn successful", Toast.LENGTH_SHORT).show()
+                var intent = Intent(this,WelcomeActivity::class.java)
+                intent.putExtra("logUser",logUser)
+                startActivity(intent)
+            }else   {
+                Toast.makeText(this, "Unexpected error happen, try again", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun isValidMailAndPassword(email: String, password: String) : Boolean{
+        return !email.isNullOrEmpty() && !password.isNullOrEmpty()
+    }
+
+
+
 
 }
+
+
