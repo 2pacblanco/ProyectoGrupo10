4e76fcb3dc73c6015b50aa1ba3e6edff84c598b7
life <ramartinez2@miuandes.cl>
2020-07-06 21:11:59 -0400

    asd

--- Proyecto/app/src/main/java/com/example/proyecto_todolist_grupo10/MainActivity.kt
+++ Proyecto/app/src/main/java/com/example/proyecto_todolist_grupo10/MainActivity.kt
@@ -11,9 +11,9 @@ import com.example.proyecto_todolist_grupo10.networking.HerokuApi
 import com.example.proyecto_todolist_grupo10.networking.HerokuApiService
 import com.example.proyecto_todolist_grupo10.util.LocationUtil
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignIn.getSignedInAccountFromIntent
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
@@ -46,7 +46,7 @@ class MainActivity : AppCompatActivity() {
         fusedLocationClient1 = LocationServices.getFusedLocationProviderClient(this)
         ItemDetail.locationData = LocationUtil(this)
 
-<<<<<<< HEAD
+
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
@@ -59,8 +59,13 @@ class MainActivity : AppCompatActivity() {
         if (user != null){
             logUser = user
         }
-=======
->>>>>>> 08a9a7808aceedfc1fbfa813537a17054b1abb4e
+
+        if (mAuth.currentUser == null){
+            Toast.makeText(this, "Se ha cerrado sesion de la ultima vez", Toast.LENGTH_SHORT).show()
+        }   else{
+            Toast.makeText(this, "Usuario ya loggeado, se cerrara sesion", Toast.LENGTH_SHORT).show()
+            mAuth.signOut()
+        }
 
         //actualizado a la api de entrega 3
         val request = HerokuApiService.buildService(HerokuApi::class.java)
@@ -100,20 +105,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun signIn() {
-        val signInIntent = mGoogleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_GOOGLE_SIGN_IN)
-    }
-
-    private fun signOut() {
-        // Firebase sign out
-        mAuth.signOut()
-
-        // Google sign out
-        mGoogleSignInClient.signOut().addOnCompleteListener(this) {
-        }
-    }
-
     private fun signInGoogle(){
         val signInIntent: Intent = mGoogleSignInClient.signInIntent
         startActivityForResult(signInIntent, RC_GOOGLE_SIGN_IN)
@@ -124,12 +115,16 @@ class MainActivity : AppCompatActivity() {
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_GOOGLE_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val task = getSignedInAccountFromIntent(data)
             try {
                 // Google Sign In was successful, authenticate with Firebase
                 val account = task.getResult(ApiException::class.java)!!
                 Toast.makeText(this, "firebaseAuthWithGoogle:", Toast.LENGTH_SHORT).show()
-                firebaseAuthWithGoogle(account.idToken!!)
+                if (account != null) {
+                    firebaseAuthWithGoogle(account.idToken!!)
+                }else{
+                    Toast.makeText(this, "cuenta vacia", Toast.LENGTH_SHORT).show()
+                }
             } catch (e: ApiException) {
                 // Google Sign In failed, update UI appropriately
                 Toast.makeText(this, "Google sign in failed", Toast.LENGTH_SHORT).show()
@@ -145,7 +140,9 @@ class MainActivity : AppCompatActivity() {
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
                     Toast.makeText(this, "signInWithCredential:success", Toast.LENGTH_SHORT).show()
-                    val user = mAuth.currentUser
+                    var intent = Intent(this,WelcomeActivity::class.java)
+                    intent.putExtra("logUser",logUser)
+                    startActivity(intent)
                 } else {
                     // If sign in fails, display a message to the user.
                     Toast.makeText(this, "Authentication Failed", Toast.LENGTH_SHORT).show()
@@ -155,6 +152,7 @@ class MainActivity : AppCompatActivity() {
                 // ...
             }
     }
+
 
     private fun logInByEmailAndPassword(email: String, password: String){
         mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this){task ->
